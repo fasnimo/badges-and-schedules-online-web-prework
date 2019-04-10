caad67956b0c4b76ba2610057b4f498dcb5d31b0
Rene Cocom <fasnimo@gmail.com>
2019-04-10 21:25:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,11 @@
 def badge_maker(name)
-  return "Hello, my name is #{name}"
+  return "Hello, my name is #{name}."
 end 
 
 def batch_badge_creator(name)
   newarray = []
   name.each do |name|
-    newarray.push("Hello, my name is #{name}")
+    newarray.push("Hello, my name is #{name}.")
   end
   return newarray
 end 
